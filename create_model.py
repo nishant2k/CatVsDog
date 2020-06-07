dd66f512ce5033c6b137098cfc3117a37576f2ef
Nishant Vedwal <56424858+nishant2k@users.noreply.github.com>
2020-06-07 21:52:00 +0530

    Update create_model.py

--- create_model.py
+++ create_model.py
@@ -11,21 +11,8 @@ DATADIR = "C:/Users/91811/Anaconda3/Desktop/kagglecatsanddogs_3367a/PetImages" #
 
 CATEGORIES = ["Dog", "Cat"]
 
-for category in CATEGORIES:  # do dogs and cats
-    path = os.path.join(DATADIR,category)  # create path to dogs and cats
-    for img in os.listdir(path):  # iterate over each image per dogs and cats
-        img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
-        plt.imshow(img_array, cmap='gray')  # graph it
-        plt.show()  # display!
-
-        break  # we just want one for now so break
-    break  #...and one more!
-    
 IMG_SIZE = 50 #resizing each images to particular size
 
-new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-plt.imshow(new_array, cmap='gray')
-plt.show()
 
 """Creating the training data for the model"""
 training_data = []
